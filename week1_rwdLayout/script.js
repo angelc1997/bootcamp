4c7be6f07d524265cd822db8545c83259f3da7ef
angelc1997 <162414402+angelc1997@users.noreply.github.com>
2024-04-03 12:50:27 +0800

    v3

--- week1_rwdLayout/script.js
+++ week1_rwdLayout/script.js
@@ -1,17 +1,9 @@
-function toggleMenu(){
-    const menu = document.querySelector('.menu');
-    const open = document.querySelector(".menu_mobile")
-    const close = document.querySelector(".menu_mobile--close")
-
-    if (menu.style.display == "none") {
-        menu.style.display = "block";
-        open.style.display = "none";
-        close.style.display = "block";
-    } else {
-        menu.style.display = "none";
-        open.style.display = "block";
-        close.style.display = "none";
-    }
-
+function showMenu(){
+  const menu = document.querySelector('.menu')
+  menu.style.display = 'flex'
 }
 
+function hideMenu(){
+  const menu = document.querySelector('.menu')
+  menu.style.display = 'none'
+}
